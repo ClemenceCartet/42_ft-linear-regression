af87fb08161c511ffc8c12d3639d12bbf7e44c0a
Clémence <clemence.cartet@geode.com>
2024-05-20 09:24:06 +0200

    min, max

--- train.py
+++ train.py
@@ -1,39 +1,36 @@
 import sys
-import os.path
+import os
 import csv
 
-
-# def normalize(min: int, max: int) -> float:
+def normalize(min: int, max: int) -> float:
+    
 
 
 if __name__ == "__main__":
     try:
-        assert (
-            len(sys.argv) == 2
-        ), "You need to enter the path of your data file!"
-        assert os.path.isfile(
-            sys.argv[1]
-        ), "There is a problem with you file path..."
+        assert len(sys.argv) == 2, "You need to enter the path of your data file!"
+        assert os.path.isfile(sys.argv[1]), "There is a problem with you file path..."
         with open(sys.argv[1], "r") as csvfile:
             read_content = csv.reader(csvfile, delimiter=",")
             next(read_content)
-            data: list[tuple[int, int]] = []
+            data: list[dict[str, int]] = []
             for km, price in read_content:
-                data.append((int(km), int(price)))
+                data.append({'km': int(km), 'price': int(price)})
         print(data)
     except AssertionError as msg:
         print(msg)
     except ValueError:
         print("There is a problem in your data file.")
+    except PermissionError:
+        print("I can't open your data file.")
 
-    # for km
-    min_km = min(data)[0]
-    max_km = max(data)[0]
-    min_price = lambda x, y: min(x, y[0])
-    max_price = max(data)[1]
+    min_km = min(row['km'] for row in data)
+    max_km = max(row['km'] for row in data)
+    min_price = min(row['price'] for row in data)
+    max_price = max(row['price'] for row in data)
+    print(min_km, max_km)
     print(min_price, max_price)
-    print(min(data))
-    # min_km = min([(y, x) for km, price in data])[::-1]
-    # key=lambda x:x[1]
+    
+    
 
     # print(f'{os.getlogin()}! How are you?')
