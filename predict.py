da8cad17309e0354bae375a89e3770083ea6ef45
Clémence Cartet <87325523+ClemenceCartet@users.noreply.github.com>
2024-05-24 11:37:58 +0200

    Add files via upload

--- predict.py
+++ predict.py
@@ -10,31 +10,49 @@ from utils import set_factors, normalize, denormalize
 def model(
     factors: np.ndarray[np.float64, np.float64], thetas: np.ndarray[np.float64]
 ) -> np.ndarray[np.float64]:
-    """Function that will calculate the estimated price of a car for a given mileage (factors)"""
+    """Function that will calculate the estimated price
+    of a car for a given mileage (factors)"""
     return factors.dot(thetas)
 
 
 if __name__ == "__main__":
     try:
-        with open("thetas.json", "r") as file:
+        with open("thetas.json", "rb") as file:
             data = json.load(file)
-            thetas = np.array(data)[:, np.newaxis]
-        while True:
-            val = input("Enter a mileage: ")
-            if val == "exit":
-                sys.exit()
-            try:
-                value = float(val)
-                if value < 0:
-                    print("You must enter a positve mileage.")
-                    continue
-                norm_value = normalize(value, 22899.0, 217101.0)
-                m_factors = set_factors([norm_value])
-                price = model(m_factors, thetas)
-                denorm_price = denormalize(price.item(), 3650.0, 4640.0)
-                print(f"The estimated price for your mileage is {denorm_price:.2f}")
-            except ValueError:
-                print("You must enter a valid mileage.")
-                continue
-    except Exception:
+            thetas = np.array(data["thetas"], dtype=float)[:, np.newaxis]
+            min_mileage, range_mileage = (
+                float(data["min_mileage"]),
+                float(data["range_mileage"]),
+            )
+            min_price, range_price = float(data["min_price"]), float(
+                data["range_price"]
+            )
+    except OSError:
         print("There is no file. You must first train your model.")
+        thetas = np.zeros((2, 1))
+        min_mileage, range_mileage = 0.0, 1.0
+        min_price, range_price = 0.0, 1.0
+    except ValueError:
+        print("There is a problem in you thetas file.")
+        sys.exit()
+
+    while True:
+        val = input("Enter a mileage: ")
+        if val == "exit":
+            sys.exit()
+        try:
+            value = float(val)
+            if value < 0:
+                print("You must enter a positve mileage.")
+                continue
+            norm_value = normalize(value, min_mileage, range_mileage)
+            m_factors = set_factors([norm_value])
+            price = model(m_factors, thetas)
+            denorm_price = denormalize(price.item(), min_price, range_price)
+            print(
+                f"The estimated price for your mileage is \
+{denorm_price:.2f}"
+            )
+        except ValueError:
+            print("You must enter a valid mileage.")
+            continue
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,7 +12,9 @@ def cost_function(
     predictions: np.ndarray[np.float64], prices: np.ndarray[np.float64]
 ) -> float:
     """Sum of all errors divided by number of data"""
-    return sum(np.power(np.subtract(predictions, prices), 2)) / (2 * len(prices))
+    return sum(np.power(np.subtract(predictions, prices), 2)) / (
+        2 * len(prices)
+    )
 
 
 def find_gradients(
@@ -36,24 +39,21 @@ def gradient_descent_algorithm(
 
 
 def training(
-    axes,
-    factors: np.ndarray[np.float64, np.float64],
-    thetas: np.ndarray[np.float64],
-    prices: np.ndarray[np.float64],
-    learning_rate: float,
+    axes, learning_rate: float, nb_iterations: int
 ) -> tuple[np.ndarray[np.float64], np.ndarray[np.float64]]:
     """Training"""
     cost_history: list[float] = []
-    thetas_history: list[np.ndarray[np.float64]] = []
+    factors = set_factors(norm_mileages)  # ndarray(m, 2)
+    thetas = np.zeros((2, 1))  # ndarray(2, 1)
+    prices = np.array(norm_prices)[:, np.newaxis]  # ndarray(m, 1)
 
-    for i in range(200):
-        thetas_history.append(thetas)
-        m_predictions = model(factors, thetas)
-        cost_history.append(cost_function(m_predictions, prices))
+    for i in range(nb_iterations):
+        predictions = model(factors, thetas)
+        cost_history.append(cost_function(predictions, prices))
         # last_two = cost_history[-2:]
         # if len(last_two) > 1 and last_two[0] - last_two[1] < 10**-6:
         #     break
-        gradients = find_gradients(m_predictions, prices, factors)
+        gradients = find_gradients(predictions, prices, factors)
         thetas = gradient_descent_algorithm(thetas, gradients, learning_rate)
 
     axes[1].plot(list(range(i + 1)), cost_history, color="red")
@@ -61,10 +61,20 @@ def training(
     axes[1].set_ylabel("Mean Squared Error")
     axes[1].set_title("Cost function")
 
-    return m_predictions, thetas
+    return predictions, thetas
 
 
 if __name__ == "__main__":
+    try:
+        nb_iterations: int = 200
+        learning_rate: float = 0.7
+        if len(sys.argv) == 3:
+            learning_rate = float(sys.argv[1])
+            nb_iterations = int(sys.argv[2])
+    except ValueError:
+        print("There is a problem in your input.")
+        sys.exit()
+
     try:
         mileages, prices = get_datas("data.csv")
 
@@ -74,26 +84,32 @@ if __name__ == "__main__":
         axes[1].grid()
 
         min_mileage, range_mileage = find_scale(mileages)
-        print(min_mileage, range_mileage)
         min_price, range_price = find_scale(prices)
-        print(min_price, range_price)
-        norm_mileages = [normalize(m, min_mileage, range_mileage) for m in mileages]
+        norm_mileages = [
+            normalize(m, min_mileage, range_mileage) for m in mileages
+        ]
         norm_prices = [normalize(n, min_price, range_price) for n in prices]
 
-        m_factors = set_factors(norm_mileages)
-        m_thetas = np.zeros((2, 1))
-        m_norm_prices = np.array(norm_prices)[:, np.newaxis]
-        m_predictions, thetas = training(axes, m_factors, m_thetas, m_norm_prices, 0.7)
+        predictions, thetas = training(axes, learning_rate, nb_iterations)
 
         with open("thetas.json", "w") as file:
-            json.dump([thetas[0].item(), thetas[1].item()], file)
+            json.dump(
+                {
+                    "thetas": [thetas[0].item(), thetas[1].item()],
+                    "min_mileage": min_mileage,
+                    "range_mileage": range_mileage,
+                    "min_price": min_price,
+                    "range_price": range_price,
+                },
+                file,
+            )
 
         axes[0].scatter(norm_mileages, norm_prices, color="blue")
-        axes[0].plot(norm_mileages, m_predictions, color="red")
+        axes[0].plot(norm_mileages, predictions, color="red")
         axes[0].set_xlabel("Mileage")
         axes[0].set_ylabel("Price")
         axes[0].set_title("Predictions with norm datas")
-        axes[0].legend(["Datas", "Predictions"])  # loc="lower right"
+        axes[0].legend(["Datas", "Predictions"])
 
         plt.show()
 
